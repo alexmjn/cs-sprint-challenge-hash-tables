b412b6c43caf38ef9d093d55f300d5dc1279f18d
alexmjn <a.jenkneary@gmail.com>
2020-07-10 11:15:27 -0400

    ex2 done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -7,8 +7,25 @@ class Ticket:
 
 def reconstruct_trip(tickets, length):
     """
-    YOUR CODE HERE
+    Understanding:
+
+    We can create a dictionary that serves as a hash table
+    containing the list of flights to and from each destination.
+    We're given "NONE" at the start, so by using that destination
+    as the key for the next source, we find our next source-destination
+    pair. We then append each of these cities as we hit them,
+    doing this process once for each ticket we have.
     """
-    # Your code here
+    trip_dict = {}
+
+    for ticket in tickets:
+        trip_dict[ticket.source] = ticket.destination
+
+    print(trip_dict)
+    route = []
+    route.append(trip_dict["NONE"])
+
+    for i in range(1, len(tickets)):
+        route.append(trip_dict[route[i-1]])
 
     return route
